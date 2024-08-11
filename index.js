25c36da774e44ac7329ca4b38fb81549e9df1d30
Takanobu Suzuki <nobublack555@gmail.com>
2024-08-11 16:00:31 -0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} value The number to clamp.
+ * @param {number} lower The lower bound.
+ * @param {number} upper The upper bound.
+ * @returns {number} Returns the clamped number.
+ */
+export default function clamp(value, lower, upper) {
+  console.log();
+  throw "Not implemented";
+}
